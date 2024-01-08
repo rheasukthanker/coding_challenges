acf78c94b949c834ba37e4184ff7619dfa80c2d6
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-01-08 07:16:03 +0100

    Update check_subarray_sum.py

--- problems/check_subarray_sum.py
+++ problems/check_subarray_sum.py
@@ -1,3 +1,9 @@
+'''Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.'''
 class Solution(object):
     def checkSubarraySum(self, nums, k):
         """
