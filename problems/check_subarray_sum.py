d1d13009679c210c486c5fb50bd2c2ec6145c4a3
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-01-08 07:14:45 +0100

    Create check_subarray_sum.py

---
+++ problems/check_subarray_sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        d = dict()
+        d[0] = -1
+        sums = 0
+        for i in range(len(nums)):
+            sums+=nums[i]
+            if(k!=0):
+                sums = sums%k
+            if(sums in d):
+                if(i-d[sums]>1):
+                    return(True)
+            else:
+                d[sums] = i
+
+        return(False)
