f858e5abed5ce99073afcbf771f5b85d75cf7379
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-01-10 06:21:07 +0100

    Create min_jump_number.py

---
+++ problems/min_jump_number.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/jump-game-ii/
+class Solution:
+  def jump(self, nums):
+    ans = 0
+    end = 0
+    farthest = 0
+
+    # Implicit BFS
+    for i in range(len(nums) - 1):
+      farthest = max(farthest, i + nums[i])
+      if farthest >= len(nums) - 1:
+        ans += 1
+        break
+      if i == end:      # Visited all the items on the current level
+        ans += 1        # Increment the level
+        end = farthest  # Make the queue size for the next level
+
+    return ans
