4e7c0074c3a01387d22620a4f713532e917deeb8
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-01-18 08:11:47 +0100

    Create valid_abbreviation.py

---
+++ problems/valid_abbreviation.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        i, j = 0, 0
+        while i < len(word) and j < len(abbr):
+            if abbr[j].isdigit():
+                cur = 0
+                while j < len(abbr) and abbr[j].isdigit():
+                    if cur == 0 and abbr[j] == '0':
+                        return False
+                    cur = cur * 10 + int(abbr[j])
+                    j += 1
+                i += cur
+            else:
+                if word[i] != abbr[j]:
+                    return False
+                i += 1
+                j += 1
+        return i == len(word) and j == len(abbr)
