d3ea2adfbf872def2a38ad7e06a0644e49fa7332
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-01-21 05:56:56 +0100

    Update subarray_with_given_sum.py

--- problems/subarray_with_given_sum.py
+++ problems/subarray_with_given_sum.py
@@ -16,3 +16,18 @@ target_sum = 6
  
 subarray = find_subarray_with_given_sum(arr, target_sum)
 print("Array Exists", subarray)
+
+# return number of sunch occurences
+class Solution:
+    def subarraySum(self, nums, k):
+        ans, n = 0, len(nums)
+        preSum = [nums[0]]
+        dic = {}
+        dic[0] = 1
+        for i in nums[1:]:
+            preSum.append(i+preSum[-1])
+        for i in preSum:
+            if i-k in dic:
+                ans+=dic[i-k]
+            dic[i] = dic.get(i,0) + 1 
+        return ans
