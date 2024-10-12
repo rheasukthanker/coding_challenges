4f9a708be8c9c0594abbc35fede41fa6c11e797f
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-10-12 17:58:57 +0200

    majority element

---
+++ arrays/majority_element.py
@@ -0,0 +1,18 @@
+'''Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.'''
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        m = defaultdict(int)
+        
+        for num in nums:
+            m[num] += 1
+        
+        n = n // 2
+        for key, value in m.items():
+            if value > n:
+                return key
+        
+        return 0
