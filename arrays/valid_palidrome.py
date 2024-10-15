85490441f4ad61ceafcff8d5087cf439e5f3b902
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-10-15 04:46:12 +0200

    Create valid_palidrome.py

---
+++ arrays/valid_palidrome.py
@@ -0,0 +1,25 @@
+'''A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+
+        return True
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        return s.lower() == s.lower()[::-1]
