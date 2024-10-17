46b59ec51e220b495f1843ea2fa864a410d5523e
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-10-17 08:03:12 +0200

    Create is_subsequence.py

---
+++ misc/is_subsequence.py
@@ -0,0 +1,13 @@
+'''You are given an integer num. You can swap two digits at most once to get the maximum valued number.
+
+Return the maximum valued number you can get.'''
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = tp = 0
+
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        
+        return sp == len(s)
