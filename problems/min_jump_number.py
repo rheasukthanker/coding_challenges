dbf65aa6a8466e76d55b485127baf137080e7a4c
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-10-24 07:12:25 +0200

    Update min_jump_number.py

--- problems/min_jump_number.py
+++ problems/min_jump_number.py
@@ -1,4 +1,10 @@
-#https://leetcode.com/problems/jump-game-ii/
+'''You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].'''
 class Solution:
   def jump(self, nums):
     ans = 0
@@ -16,3 +22,18 @@ class Solution:
         end = farthest  # Make the queue size for the next level
 
     return ans
+    
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        near = far = jumps = 0
+
+        while far < len(nums) - 1:
+            farthest = 0
+            for i in range(near, far + 1):
+                farthest = max(farthest, i + nums[i])
+            
+            near = far + 1
+            far = farthest
+            jumps += 1
+        
+        return jumps
