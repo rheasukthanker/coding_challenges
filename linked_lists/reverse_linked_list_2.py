6c98a4493c6b245349250f45f0ca8c8220846348
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-11-02 05:50:03 +0100

    Create reverse_linked_list_2.py

---
+++ linked_lists/reverse_linked_list_2.py
@@ -0,0 +1,21 @@
+'''Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.'''
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
+
+        cur = prev.next
+        for _ in range(right - left):
+            temp = cur.next
+            cur.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
