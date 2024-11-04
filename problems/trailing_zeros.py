153b581b9c71cd018df63bdfa9179648cc546ab5
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-11-04 03:21:17 +0100

    Create trailing_zeros.py

---
+++ problems/trailing_zeros.py
@@ -0,0 +1,11 @@
+'''Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.'''
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n > 0:
+            n //= 5
+            count += n
+        return count
