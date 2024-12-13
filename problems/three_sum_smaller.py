c736ec3791d87a3f7b3d771e73a620d3932741ac
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-13 13:23:51 +0100

    Update three_sum_smaller.py

--- problems/three_sum_smaller.py
+++ problems/three_sum_smaller.py
@@ -1,3 +1,6 @@
+#Given an array of n integers nums and an integer target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target
+# The time complexity is O(n**2), and the space complexity is O(log(n)). Here, n is the length of the array nums
+
 class Solution(object):
  def threeSumSmaller(self, nums, target):
     count = 0
