ce3772a7f1ce1390e848fe4b248bd3f5d43c970f
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-23 17:09:46 +0100

    Create 3sumclosest.py

---
+++ meta/3sumclosest.py
@@ -0,0 +1,24 @@
+'''Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.'''
+# TC: O(n**2) SC O(logn) to O(n), depending on the implementation of the sorting algorithm.
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = float("inf")
+        nums.sort()
+        for i in range(len(nums)):
+            lo, hi = i + 1, len(nums) - 1
+            while lo < hi:
+                sum = nums[i] + nums[lo] + nums[hi]
+                if abs(target - sum) < abs(diff):
+                    diff = target - sum
+                if sum < target:
+                    lo += 1
+                else:
+                    hi -= 1
+            if diff == 0:
+                break
+            
+        return target - diff
