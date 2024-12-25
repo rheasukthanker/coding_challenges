b038f28aa213cfb334c7430805cf014ac44eab6c
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-25 13:56:02 +0100

    Create moving_average.py

---
+++ meta/moving_average.py
@@ -0,0 +1,26 @@
+'''Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Implement the MovingAverage class:
+
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.'''
+#Time Complexity: O(1).
+#Space Complexity: O(N), where N is the size of the moving window.
+from collections import deque
+class MovingAverage:
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = deque()
+        # number of elements seen so far
+        self.window_sum = 0
+        self.count = 0
+
+    def next(self, val: int) -> float:
+        self.count += 1
+        # calculate the new sum by shifting the window
+        self.queue.append(val)
+        tail = self.queue.popleft() if self.count > self.size else 0
+
+        self.window_sum = self.window_sum - tail + val
+
+        return self.window_sum / min(self.size, self.count)
