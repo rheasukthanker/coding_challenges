5aad5e6e4dcecfdc3e0a9e2685361ea6ce39a596
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-26 06:33:06 +0100

    Create squares_of_sorted.py

---
+++ meta/squares_of_sorted.py
@@ -0,0 +1,17 @@
+#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+#TC O(n), SC O(n) including output O(1) otherwise
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
