038b2315f2a43f59db38c204540bbecb9f1ab280
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-26 06:42:03 +0100

    Create missing_ranges.py

---
+++ meta/missing_ranges.py
@@ -0,0 +1,33 @@
+'''You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are within the inclusive range.
+
+A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+
+Return the shortest sorted list of ranges that exactly covers all the missing numbers. That is, no element of nums is included in any of the ranges, and each missing number is covered by one of the ranges.
+
+'''
+#TC O(n), SC O(1)
+class Solution:
+    def findMissingRanges(
+        self, nums: List[int], lower: int, upper: int
+    ) -> List[List[int]]:
+        n = len(nums)
+        missing_ranges = []
+        if n == 0:
+            missing_ranges.append([lower, upper])
+            return missing_ranges
+
+        # Check for any missing numbers between the lower bound and nums[0].
+        if lower < nums[0]:
+            missing_ranges.append([lower, nums[0] - 1])
+
+        # Check for any missing numbers between successive elements of nums.
+        for i in range(n - 1):
+            if nums[i + 1] - nums[i] <= 1:
+                continue
+            missing_ranges.append([nums[i] + 1, nums[i + 1] - 1])
+
+        # Check for any missing numbers between the last element of nums and the upper bound.
+        if upper > nums[n - 1]:
+            missing_ranges.append([nums[n - 1] + 1, upper])
+
+        return missing_ranges
