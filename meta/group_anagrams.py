5c2cb7795c46f83671d31288e2dd69b3fc70614f
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-29 03:03:03 +0100

    Create group_anagrams.py

---
+++ meta/group_anagrams.py
@@ -0,0 +1,19 @@
+'''Given an array of strings strs, group the anagrams together. You can return the answer in any order.'''
+#TC: O(nklogk), SC:O(nk)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return list(ans.values())
+#TC: O(nk), SC:O(nk)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values())
+
