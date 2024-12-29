3d6b550a7e91665c1550192ac8fe5d6be999c938
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-29 07:01:01 +0100

    Create search_in_rotated_sorted_array.py

---
+++ meta/search_in_rotated_sorted_array.py
@@ -0,0 +1,33 @@
+'''There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.'''
+#TC: O(logn), SC O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            # Case 1: find target
+            if nums[mid] == target:
+                return mid
+
+            # Case 2: subarray on mid's left is sorted
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # Case 3: subarray on mid's right is sorted.
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
