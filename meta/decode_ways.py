f3105677690923bb90864bb533c0e8bebd3cfb15
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-29 13:47:56 +0100

    Create decode_ways.py

---
+++ meta/decode_ways.py
@@ -0,0 +1,98 @@
+'''You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+
+"1" -> 'A'
+
+"2" -> 'B'
+
+...
+
+"25" -> 'Y'
+
+"26" -> 'Z'
+
+However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
+
+For example, "11106" can be decoded into:
+
+"AAJF" with the grouping (1, 1, 10, 6)
+"KJF" with the grouping (11, 10, 6)
+The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+Note: there may be strings that are impossible to decode.
+
+Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+'''
+# TC: O(n) SC O(n)
+'''Time Complexity: O(N), where N is length of the string. Memoization helps in pruning the recursion tree and hence decoding for an index only once. Thus this solution is linear time complexity.
+
+Space Complexity: O(N). The dictionary used for memoization would take the space equal to the length of the string. There would be an entry for each index value. The recursion stack would also be equal to the length of the string.'''
+class Solution:
+
+    @lru_cache(maxsize=None)
+    def recursiveWithMemo(self, index, s) -> int:
+        # If you reach the end of the string
+        # Return 1 for success.
+        if index == len(s):
+            return 1
+
+        # If the string starts with a zero, it can't be decoded
+        if s[index] == "0":
+            return 0
+
+        if index == len(s) - 1:
+            return 1
+
+        answer = self.recursiveWithMemo(index + 1, s)
+        if int(s[index : index + 2]) <= 26:
+            answer += self.recursiveWithMemo(index + 2, s)
+
+        return answer
+
+    def numDecodings(self, s: str) -> int:
+        return self.recursiveWithMemo(0, s)
+# TC: O(n), SC: O(n)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # Array to store the subproblem results
+        if s[0]=="0":
+            return 0
+        dp = [0 for _ in range(len(s) + 1)]
+
+        dp[0] = 1
+        # Ways to decode a string of size 1 is 1. Unless the string is '0'.
+        # '0' doesn't have a single digit decode.
+        dp[1] = 0 if s[0] == "0" else 1
+
+        for i in range(2, len(dp)):
+
+            # Check if successful single digit decode is possible.
+            if s[i - 1] != "0":
+                dp[i] = dp[i - 1]
+
+            # Check if successful two digit decode is possible.
+            two_digit = int(s[i - 2 : i])
+            if two_digit >= 10 and two_digit <= 26:
+                dp[i] += dp[i - 2]
+
+        return dp[len(s)]
+  # TC: O(n) SC: O(1)
+  class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0":
+            return 0
+
+        two_back = 1
+        one_back = 1
+        for i in range(1, len(s)):
+            current = 0
+            if s[i] != "0":
+                current = one_back
+            two_digit = int(s[i - 1 : i + 1])
+            if two_digit >= 10 and two_digit <= 26:
+                current += two_back
+            two_back = one_back
+            one_back = current
+
+        return one_back
