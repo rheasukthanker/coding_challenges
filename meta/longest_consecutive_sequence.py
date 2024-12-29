1435993c2c59bf1bf84bff7980f2b69b572b3156
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-29 14:02:45 +0100

    Create longest_consecutive_sequence.py

---
+++ meta/longest_consecutive_sequence.py
@@ -0,0 +1,37 @@
+'''Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.'''
+'''Time complexity : O(n).
+
+Although the time complexity appears to be quadratic due to the while
+loop nested within the for loop, closer inspection reveals it to be
+linear. Because the while loop is reached only when currentNum marks
+the beginning of a sequence (i.e. currentNum-1 is not present in
+nums), the while loop can only run for n iterations throughout the
+entire runtime of the algorithm. This means that despite looking like
+O(n⋅n) complexity, the nested loops actually run in O(n+n)=O(n)
+time. All other computations occur in constant time, so the overall
+runtime is linear.
+
+Space complexity : O(n).
+
+In order to set up O(1) containment lookups, we allocate linear space
+for a hash table to store the O(n) numbers in nums. Other than that,
+the space complexity is identical to that of the brute force solution.'''
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
