28194cbacbca2b928a6904b3fc2431db369e4647
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2024-12-29 12:55:57 +0100

    Create climbing_stairs.py

---
+++ meta/climbing_stairs.py
@@ -0,0 +1,35 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+
+#Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# TC O(n) SC: O(n) for SC: O(1), simply use fibonacci numbers
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        first = 1
+        second = 2
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+        return second
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        first = 1
+        second = 2
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+        return second
