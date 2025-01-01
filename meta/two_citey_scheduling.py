dda6d2d1045b8234fb89d648c758ed950e3a29ed
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-01-02 00:52:48 +0100

    Create two_citey_scheduling.py

---
+++ meta/two_citey_scheduling.py
@@ -0,0 +1,15 @@
+'''A company is planning to interview 2n people. Given the array costs where costs[i] = [aCosti, bCosti], the cost of flying the ith person to city a is aCosti, and the cost of flying the ith person to city b is bCosti.'''
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        # Sort by a gain which company has 
+        # by sending a person to city A and not to city B
+        costs.sort(key = lambda x : x[0] - x[1])
+        
+        total = 0
+        n = len(costs) // 2
+        # To optimize the company expenses,
+        # send the first n persons to the city A
+        # and the others to the city B
+        for i in range(n):
+            total += costs[i][0] + costs[i + n][1]
+        return total
