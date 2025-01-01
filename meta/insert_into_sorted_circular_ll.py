aea77249f0673ed8386cc2353371a780eba774da
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-01-01 05:25:03 +0100

    Create insert_into_sorted_circular_ll.py

---
+++ meta/insert_into_sorted_circular_ll.py
@@ -0,0 +1,48 @@
+'''Given a Circular Linked List node, which is sorted in non-descending order, write a function to insert a value insertVal into the list such that it remains a sorted circular list. The given node can be a reference to any single node in the list and may not necessarily be the smallest value in the circular list.
+
+If there are multiple suitable places for insertion, you may choose any place to insert the new value. After the insertion, the circular list should remain sorted.
+
+If the list is empty (i.e., the given node is null), you should create a new single circular list and return the reference to that single node. Otherwise, you should return the originally given node.'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+# TC: O(N), SC: O(1)
+class Solution:
+    def insert(self, head: 'Node', insertVal: int) -> 'Node':
+
+        if head == None:
+            newNode = Node(insertVal, None)
+            newNode.next = newNode
+            return newNode
+ 
+        prev, curr = head, head.next
+        toInsert = False
+
+        while True:
+            
+            if prev.val <= insertVal <= curr.val:
+                # Case #1.
+                toInsert = True
+            elif prev.val > curr.val:
+                # Case #2. where we locate the tail element
+                # 'prev' points to the tail, i.e. the largest element!
+                if insertVal >= prev.val or insertVal <= curr.val:
+                    toInsert = True
+
+            if toInsert:
+                prev.next = Node(insertVal, curr)
+                # mission accomplished
+                return head
+
+            prev, curr = curr, curr.next
+            # loop condition
+            if prev == head:
+                break
+        # Case #3.
+        # did not insert the node in the loop
+        prev.next = Node(insertVal, curr)
+        return head
