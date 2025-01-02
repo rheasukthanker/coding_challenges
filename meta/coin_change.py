aa9741db47d7e2f2a86faf5dec6436641e4b954d
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-01-02 01:17:44 +0100

    Create coin_change.py

---
+++ meta/coin_change.py
@@ -0,0 +1,26 @@
+'''You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.'''
+# TC: O(S*n), SC: O(S)
+from functools import lru_cache
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        @lru_cache(None)
+        def dfs(rem):
+            if rem < 0:
+                return -1
+            if rem == 0:
+                return 0
+            min_cost = float('inf')
+            for coin in coins:
+                res = dfs(rem - coin)
+                if res != -1:
+                    min_cost = min(min_cost, res + 1)
+            return min_cost if min_cost != float('inf') else -1
+
+        return dfs(amount)
