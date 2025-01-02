17ab4ccbee6e99a99e3f8d7b2256e09a52c18ecb
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-01-02 05:51:52 +0100

    Update targetsum.py

--- meta/targetsum.py
+++ meta/targetsum.py
@@ -5,6 +5,7 @@ You want to build an expression out of nums by adding one of the symbols '+' and
 For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
 Return the number of different expressions that you can build, which evaluates to target.'''
 # Total sum = m TC: O(nm), SC: O(m)
+#https://leetcode.com/problems/target-sum/
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = defaultdict(int)
