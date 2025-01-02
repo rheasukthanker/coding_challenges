2310e270de6cf184dddf6a4b21411044217ba369
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-01-02 13:39:51 +0100

    Create age_aapropriate_friends.py

---
+++ meta/age_aapropriate_friends.py
@@ -0,0 +1,31 @@
+'''There are n persons on a social media website. You are given an integer array ages where ages[i] is the age of the ith person.
+
+A Person x will not send a friend request to a person y (x != y) if any of the following conditions is true:
+
+age[y] <= 0.5 * age[x] + 7
+age[y] > age[x]
+age[y] > 100 && age[x] < 100
+Otherwise, x will send a friend request to y.
+
+Note that if x sends a request to y, y will not necessarily send a request to x. Also, a person will not send a friend request to themself.
+
+Return the total number of friend requests made.
+
+'''
+# TC: O(A**2+N), SC: O(A). A is total number of ages, N is number of people
+class Solution(object):
+    def numFriendRequests(self, ages):
+        count = [0] * 121
+        for age in ages:
+            count[age] += 1
+
+        ans = 0
+        for ageA, countA in enumerate(count):
+            for ageB, countB in enumerate(count):
+                if ageA * 0.5 + 7 >= ageB: continue
+                if ageA < ageB: continue
+                if ageA < 100 < ageB: continue
+                ans += countA * countB
+                if ageA == ageB: ans -= countA
+
+        return ans
