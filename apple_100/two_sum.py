f17ea87cd01a047147c5c013953444335b7bdc67
Rhea Sukthanker <20137590+rheasukthanker@users.noreply.github.com>
2025-11-25 20:02:06 +0530

    Implement twoSum function to find indices

---
+++ apple_100/two_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complements = {}
+        for i,n in enumerate(nums):
+            if n in complements:
+               return  [complements[n],i]
+            complements[target-n]=i
